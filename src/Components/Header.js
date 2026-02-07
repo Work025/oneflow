37c46f70cc186cb4d13e2f41d2435006b4083978
Fozilxon <fotaxonov0408@gamil.com>
2026-02-07 17:27:00 +0500

    Prepare for GitHub Pages deploy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,40 @@
-import logo from "../Assets/Logo.svg"
-import "../Styles/Header.css"
+import React, { useState } from "react";
+import logo from "../Assets/Logo.svg";
+import "../Styles/Header.css";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className="header">
+        <header className="header">
             <div className="header-logo">
                 <img src={logo} alt="header logo" />
             </div>
 
-            <ul className="header-ul">
-                <li><a href="#">Why Oneflow?</a></li>
-                <li><a href="#">Learn</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Blog</a></li>
-                <button>Get a demo</button>
-                <button>Log in</button>
-                <i className="fas fa-earth"></i>
-            </ul>
-        </div>
-    )
+            <nav className={`header-nav ${isOpen ? "open" : ""}`}>
+                <ul className="header-ul">
+                    <li><a href="#">Why Oneflow?</a></li>
+                    <li><a href="#">Learn</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <button>Get a demo</button>
+                    <button>Log in</button>
+                    <i className="fas fa-earth" aria-hidden="true"></i>
+                </ul>
+            </nav>
+
+            <div className="hamburger" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
